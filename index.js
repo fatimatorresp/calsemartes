9a7c2b9665bfd593e28f4a24e26d1b9964a5e1d4
fatima <99816802+fatimatorresp@users.noreply.github.com>
2023-08-29 23:48:53 +0000

    enprolijar

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     console.log('entró a /')
     res.send('Hola, mundo!')
 })
- app.post('/producto', (req, res) => {  //crear
+app.post('/producto', (req, res) => {  //crear
     console.log('entró a post/producto',req.body)
     productos.push(req.body)
     fs.writeFileSync('./archivo_productos.json',JSON.stringify(productos),{encoding:'utf-8',flag:'w'});
@@ -24,32 +24,32 @@ app.get('/productos', (req, res) => { //leer
     console.log('entró a /', productos) //muestra en consola
     res.send( productos)//muestra cliente
 })
-app.get('/producto1', (req, res) => { //leer
-    console.log('entró a get/1elemento', req.body) //muestra en consola 1 elemento
-    res.send( productos[req.body.indice])//muestra cliente
-})
-app.get('/producto1/:indice', (req, res) => { //leer
+// app.get('/producto1', (req, res) => { //leer
+//     console.log('entró a get/1elemento', req.body) //muestra en consola 1 elemento
+//     res.send( productos[req.body.indice])//muestra cliente
+// })
+app.get('/producto/:indice', (req, res) => { //leer
     console.log('entró a get/producto1', productos[req.params.indice]) //muestra en consola 1 elemento
     res.send( productos[req.params.indice])//muestra cliente
 })
-app.get('/producto2', (req, res) => { //leer
+app.get('/producto', (req, res) => { //leer
     console.log('entró a get/producto2', req.query.indice) //muestra en consola 1 elemento
     res.send( productos[req.query.indice])//muestra cliente
 })
 
-app.delete('/eliminar', (req, res) => { //eliminar
+app.delete('/producto', (req, res) => { //eliminar
     console.log('entró a delete/eliminar', req.body) //muestra en consola
     productos.splice(req.body.indice,1)
     fs.writeFileSync('./archivo_productos.json',JSON.stringify(productos),{encoding:'utf-8',flag:'w'});
     res.send("se elimino ")//muestra cliente
 })
-app.get('/eliminar/:indice', (req, res) => { //eliminar
+app.get('/eliminar/producto/:indice', (req, res) => { //eliminar
     console.log('entró a eliminarindice', productos[req.params.indice]) //muestra en consola
     productos.splice(req.params.indice,1)
     fs.writeFileSync('./archivo_productos.json',JSON.stringify(productos),{encoding:'utf-8',flag:'w'});
     res.send("se elimino por cliente")//muestra cliente
 })
-app.get('/eliminar', (req, res) => { //eliminar
+app.get('/eliminar/producto', (req, res) => { //eliminar
     console.log('entró a eliminarindice', req.query.indice) //muestra en consola
     productos.splice(req.query.indice,1)
     fs.writeFileSync('./archivo_productos.json',JSON.stringify(productos),{encoding:'utf-8',flag:'w'});
